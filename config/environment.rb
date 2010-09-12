2b7683cb3a0df0d9c8f44c79264cfa773f4a80c3
ladislav.martincik <ladislav.martincik@xing.com>
2010-09-12 20:32:31 +0200

    Deprecated stuff for Rails 3

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails::Initializer.run do |config|
   # -- all .rb files in that directory are automatically loaded.
 
   # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/app/mailers )
+  config.autoload_paths += %W( #{RAILS_ROOT}/app/mailers )
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
