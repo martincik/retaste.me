b0e1ad977798fdc6adc60aca66d43c3acac33a04
ladislav.martincik <ladislav.martincik@xing.com>
2010-09-12 20:37:01 +0200

    Remove obsolete stuff for pre-bundler

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-Rails::VendorGemSourceIndex.silence_spec_warnings = true
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
